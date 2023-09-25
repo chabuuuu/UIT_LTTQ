59a08df87d8e24d24272cea156c5d03517a1fc46
chabuuuu <87564111+chabuuuu@users.noreply.github.com>
2023-09-25 16:06:14 +0700

    Object intializer

---
+++ BaiTap/Chuong02_HaPhuThinh_22521405/KhoiTaoGiaTrIChoThuocTinh_ObjectIntializer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhoiTaoGiaTrIChoThuocTinh_ObjectIntializer
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person() { Name = "John", Age = 25 };
+            Console.WriteLine("Name: " + person.Name);
+            Console.WriteLine("Age: " + person.Age);
+            Console.ReadKey();
+        }
+    }
+}

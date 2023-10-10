253af15e16ce8181e30ac31240e08da9fe811833
chabuuuu <87564111+chabuuuu@users.noreply.github.com>
2023-10-10 10:16:42 +0700

    Add chuong3 bai tap

---
+++ BaiTap/Chuong3_HaPhuThinh_22521405/BaiTap_PhepToanPhanSo/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap_PhepToanPhanSo
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCong_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                PhanSo phanso1 = this.readPhanSo1();
+                PhanSo phanso2 = this.readPhanSo2();
+                PhanSo ketqua = phanso1 + phanso2;
+                this.showKetQua(ketqua);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message);
+            }
+        }
+        private PhanSo readPhanSo1()
+        {
+            int tuPhanSo1 = int.Parse(txtTuPhanSo1.Text);
+            int mauPhanSo1 = int.Parse(txtMauPhanSo1.Text);
+            if (mauPhanSo1 == 0)
+            {
+                throw new Exception("Mẫu phân số 1 phải khác 0");
+            }
+            return new PhanSo(tuPhanSo1, mauPhanSo1);
+        }
+        private PhanSo readPhanSo2()
+        {
+            int tuPhanSo2 = int.Parse(txtTuPhanSo2.Text);
+            int mauPhanSo2 = int.Parse(txtMauPhanSo2.Text);
+            if (mauPhanSo2 == 0)
+            {
+                throw new Exception("Mẫu phân số 2 phải khác 0");
+            }
+            return new PhanSo(tuPhanSo2, mauPhanSo2);
+        }
+        private void showKetQua(PhanSo ketqua)
+        {
+            txtTuKetQua.Text = ketqua.getTu().ToString();
+            txtMauKetQua.Text = ketqua.getMau().ToString();
+        }
+
+        private void BtnTru_Click(object sender, EventArgs e)
+        {
+            try {
+                PhanSo phanso1 = this.readPhanSo1();
+                PhanSo phanso2 = this.readPhanSo2();
+                PhanSo ketqua = phanso1 - phanso2;
+                this.showKetQua(ketqua);
+            } catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message);
+            }
+        }
+
+        private void BtnNhan_Click(object sender, EventArgs e)
+        {
+            try {
+                PhanSo phanso1 = this.readPhanSo1();
+                PhanSo phanso2 = this.readPhanSo2();
+                PhanSo ketqua = phanso1 * phanso2;
+                this.showKetQua(ketqua);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message);
+            }
+        }
+
+        private void BtnChia_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                PhanSo phanso1 = this.readPhanSo1();
+                PhanSo phanso2 = this.readPhanSo2();
+                PhanSo ketqua = phanso1 / phanso2;
+                this.showKetQua(ketqua);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message);
+            }
+        }
+
+        private void txtMauPhanSo1_TextChanged(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+            if (control.Text.Length > 0)
+            {
+                if (int.Parse(control.Text) == 0)
+                {
+                    this.errorProvider1.SetError(control, "Mẫu phải khác 0");
+                }
+                else
+                {
+                    this.errorProvider1.Clear();
+                }
+            }
+        }
+
+        private void txtMauPhanSo2_TextChanged(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+            if (control.Text.Length > 0)
+            {
+                if (int.Parse(control.Text) == 0)
+                {
+                    this.errorProvider1.SetError(control, "Mẫu phải khác 0");
+                }
+                else
+                {
+                    this.errorProvider1.Clear();
+                }
+            }
+
+        }
+    }
+    class PhanSo
+    {
+        private static int GCD(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
+        }
+        public static PhanSo operator +(PhanSo a, PhanSo b)
+        {
+            if (a.mau == b.mau)
+            {
+                return new PhanSo(a.tu + b.tu, a.mau);
+            }
+            int tu1 = a.tu * b.mau;
+            int tu2 = b.tu * a.mau;
+            return new PhanSo(tu1 + tu2, a.mau * b.mau);
+        }
+        public static PhanSo operator -(PhanSo a, PhanSo b)
+        {
+            if (a.mau == b.mau)
+            {
+                return new PhanSo(a.tu - b.tu, a.mau);
+            }
+            int tu1 = a.tu * b.mau;
+            int tu2 = b.tu * a.mau;
+            return new PhanSo(tu1 - tu2, a.mau * b.mau);
+        }
+        public static PhanSo operator *(PhanSo a, PhanSo b)
+        {
+            int newTu = a.tu * b.tu;
+            int newMau = a.mau * b.mau;
+            int gcd = GCD(newMau, newTu);
+            return new PhanSo(newTu / gcd, newMau / gcd);
+        }
+        public static PhanSo operator /(PhanSo a, PhanSo b)
+        {
+            int newTu = a.tu * b.mau;
+            int newMau = a.mau * b.tu;
+            int gcd = GCD(newMau, newTu);
+            return new PhanSo(newTu / gcd, newMau / gcd);
+        }
+        public PhanSo(int tu, int mau)
+        {
+            int gcd = GCD(tu, mau);
+            this.tu = tu / gcd;
+            this.mau = mau / gcd;
+        }
+        public void xuat()
+        {
+            Console.WriteLine("{0}/{1}", this.tu, this.mau);
+        }
+        public int getTu()
+        {
+            return this.tu;
+        }
+        public int getMau()
+        {
+            return this.mau;
+        }
+
+        private int tu;
+        private int mau;
+    }
+}
---
+++ BaiTap/Chuong3_HaPhuThinh_22521405/congtru/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace congtru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NUMBER 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(382, 157);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(119, 22);
+            this.txtNum1.TabIndex = 1;
+            this.txtNum1.TextChanged += new System.EventHandler(this.txtNum1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NUMBER 2";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(382, 211);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(119, 22);
+            this.txtNum2.TabIndex = 3;
+            this.txtNum2.TextChanged += new System.EventHandler(this.txtNum2_TextChanged);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(426, 283);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 4;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
